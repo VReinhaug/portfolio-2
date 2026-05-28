4ae81f663da5a64a2c9ea95ccf1ff313ee0f4e64
VReinhaug <v.reinhaug@gmail.com>
2026-05-28 18:40:58 +0200

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Header />
 
-      <main></main>
+      <main>
+        <About />
+      </main>
     </>
   );
 }
