b8d9931eace6d4cf4590d017882de7cfa8b41390
VReinhaug <v.reinhaug@gmail.com>
2026-05-31 17:38:07 +0200

    Styling improvements project cards

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -9,10 +9,13 @@ const projects = [
   {
     title: "GoodBid",
     //image: GoodBid,
-    description:
-      "GoodBid is a semester project created as an assignment for Front-End Development studies at Noroff. The application is an auction platform where users can register using a Noroff email address, create listings for items they want to sell, and place bids on listings created by other users.",
+    description: [
+      "GoodBid is a semester project created as an assignment for Front-End Development studies at Noroff.",
 
-    techStack: "HTML5, SCSS, JavaScript, Bootstrap, API",
+      "The application is an auction platform where users can register using a Noroff email address, create listings for items they want to sell, and place bids on listings created by other users.",
+    ],
+
+    techStack: ["HTML5", "SCSS", "JavaScript", "Bootstrap", "API"],
 
     github: "https://github.com/VReinhaug/SemesterProject2",
 
@@ -22,10 +25,13 @@ const projects = [
   {
     title: "UniMart",
     //image: UniMart,
-    description:
-      "UniMart is a Course Assignment for JavaScript Frameworks as part of the Front-end Development studies at Noroff. The project is a React-based online store that allows users to browse products, view detailed product information, add items to a shopping cart, and complete a checkout flow.",
+    description: [
+      "UniMart is a Course Assignment for JavaScript Frameworks as part of the Front-end Development studies at Noroff.",
 
-    techStack: "React, JavaScript, CSS, API",
+      "The project is a React-based online store that allows users to browse products, view detailed product information, add items to a shopping cart, and complete a checkout flow.",
+    ],
+
+    techStack: ["React", "JavaScript", "CSS", "API"],
 
     github: "https://github.com/NoroffFEU/jsfw-2025-v1-veronika-jsfw",
 
@@ -35,10 +41,13 @@ const projects = [
   {
     title: "Holidaze",
     //image: Holidaze,
-    description:
-      "Holidaze is a React-based web application for booking and managing venues for overnight stays. This project was developed as the Project Exam 2 assignment during the Front-end Development program at Noroff. Users can browse venues, view detailed venue information, register an account, and make bookings. Users can also register as Venue Managers, allowing them to create, update, and delete venues, as well as manage bookings for their venues.",
+    description: [
+      "Holidaze is a React-based web application for booking and managing venues for overnight stays. This project was developed as the Project Exam 2 assignment during the Front-end Development program at Noroff.",
 
-    techStack: "React, JavaScript, Bootstrap, SASS, Yup, API",
+      "Users can browse venues, view detailed venue information, register an account, and make bookings. Users can also register as Venue Managers, allowing them to create, update, and delete venues, as well as manage bookings for their venues.",
+    ],
+
+    techStack: ["React", "JavaScript", "Bootstrap", "SASS", "Yup", "API"],
 
     github: "https://github.com/VReinhaug/project-exam-2",
 
@@ -64,12 +73,17 @@ function Projects() {
               <div className={styles.content}>
                 <h3>{project.title}</h3>
 
-                <p>{project.description}</p>
-
-                <div className={styles.techStack}>
-                  <span>{project.techStack}</span>
+                <div className={styles.description}>
+                  {project.description.map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
                 </div>
 
+                <div className={styles.techStack}>
+                  {project.techStack.map((tech) => (
+                    <span key={tech}>{tech}</span>
+                  ))}
+                </div>
                 <div className={styles.links}>
                   <a
                     href={project.github}
